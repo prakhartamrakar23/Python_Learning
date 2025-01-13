93fa71ee9f4546519cdc93d4af1206006d693f2b
prakhartamrakar23 <prakhartamrakar99@gmail.com>
2025-01-13 21:35:41 +0530

    my first python program

---
+++ HelloWorld.py
@@ -0,0 +1,10 @@
+print("Hello, World")
+type("Hello,World")
+#concenate string 
+print("hello"+" prakhar")
+#new line
+print("hello\nworld\npython")
+'''if we add the space before the print function then it gives the indentation error'''
+
+# input function used to take input from user
+input("what is your name?")
